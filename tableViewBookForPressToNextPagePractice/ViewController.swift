7240c00dffe7ac04a5c269f33c76ef4453eed6ee
Yenci <a278862002@gmail.com>
2024-10-08 10:08:24 +0800

    Initial Commit

---
+++ tableViewBookForPressToNextPagePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewBookForPressToNextPagePractice
+//
+//  Created by 陳妍慈 on 2024/10/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
